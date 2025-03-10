9880ff2dc7cb1bb4b31c18d26757a5f47380de28
Luka <lukaneek@gmail.com>
2025-03-10 17:40:13 -0500

    update

---
+++ src/main/java/com/example/demo/Config/webConfig.java
@@ -0,0 +1,19 @@
+package com.example.demo.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class webConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Map '/uploads/coverImage/**' to the physical directory 'uploads/coverImage/'
+        registry.addResourceHandler("/uploads/cover_pictures/**")
+                .addResourceLocations("file:uploads/cover_pictures/");
+    }
+}
+
+
