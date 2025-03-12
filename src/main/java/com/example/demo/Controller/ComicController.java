45d606719721e2d237df5a19ce6df33f5e4b7e01
Luka <lukaneek@gmail.com>
2025-03-12 13:33:13 -0500

    update

--- src/main/java/com/example/demo/Controller/ComicController.java
+++ src/main/java/com/example/demo/Controller/ComicController.java
@@ -7,6 +7,7 @@ import java.nio.file.StandardCopyOption;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -38,6 +39,9 @@ import jakarta.validation.Valid;
 @Controller
 public class ComicController {
 
+	@Value("${app.url.path}")
+	String appUrlPath;
+	
 	@Autowired
 	UserService userService;
 
