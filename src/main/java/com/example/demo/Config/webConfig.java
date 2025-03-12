cbcee9599905e479db5a56e9fb5c0756127bbdd6
Luka <lukaneek@gmail.com>
2025-03-12 11:20:11 -0500

    update

--- src/main/java/com/example/demo/Config/webConfig.java
+++ src/main/java/com/example/demo/Config/webConfig.java
@@ -1,11 +1,8 @@
 package com.example.demo.Config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.method.HandlerTypePredicate;
-import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 
 
 @Configuration
