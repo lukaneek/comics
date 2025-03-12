08f4ec1b51f6720ae9ff16588f89835be0957dd7
Luka <lukaneek@gmail.com>
2025-03-12 11:22:05 -0500

    update

--- src/main/java/com/example/demo/Config/webConfig.java
+++ src/main/java/com/example/demo/Config/webConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class webConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
